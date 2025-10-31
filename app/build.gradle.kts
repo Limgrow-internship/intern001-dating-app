8e817d33c69db59f69f23e2bc425d39f1538aaf1
vunguyenDev92 <vunguyendev92@gmail.com>
2025-10-31 14:04:19 +0700

    fix: delete duplicate in build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    testImplementation(libs.junit)
 }
 
 // KtLint configuration
