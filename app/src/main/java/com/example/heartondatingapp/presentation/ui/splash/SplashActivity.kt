6638fbda683a986e4eb9c6ab5abfa68d608fd5d3
Huy10010 <huynhtangnhathuy@gmail.com>
2025-11-04 14:41:32 +0700

    UI_Splash

--- app/src/main/java/com/example/heartondatingapp/presentation/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/heartondatingapp/presentation/ui/splash/SplashActivity.kt
@@ -1,3 +1,12 @@
 package com.example.heartondatingapp.presentation.ui.splash
 
-class SplashActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.heartondatingapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
