9e2121258b1b251507548e84250cc1034290771b
Huy10010 <huynhtangnhathuy@gmail.com>
2025-11-05 15:19:27 +0700

    UI_Splash

--- app/src/main/java/com/intern001/dating/presentation/ui/onboard/OnboardFragment.kt
+++ app/src/main/java/com/intern001/dating/presentation/ui/onboard/OnboardFragment.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.heartondatingapp.R
 import com.intern001.dating.MainActivity
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class OnboardFragment : Fragment() {
     override fun onCreateView(
@@ -26,8 +24,7 @@ class OnboardFragment : Fragment() {
 
         (activity as? MainActivity)?.hideBottomNavigation(true)
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            delay(4000)
+        view.findViewById<Button>(R.id.btnNext)?.setOnClickListener {
             findNavController().navigate(R.id.action_onboard_to_home)
         }
     }
