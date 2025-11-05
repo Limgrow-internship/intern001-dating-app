7299e7e501415b2551bfa10293c54d45165fc2e4
Huy10010 <huynhtangnhathuy@gmail.com>
2025-11-05 13:05:33 +0700

    Fix ktlint format errors

--- app/src/main/java/com/intern001/dating/presentation/ui/onboard/OnboardFragment.kt
+++ app/src/main/java/com/intern001/dating/presentation/ui/onboard/OnboardFragment.kt
@@ -16,7 +16,7 @@ class OnboardFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(R.layout.fragment_onboard, container, false)
     }
